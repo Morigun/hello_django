d4a9c2ffb1ee0b2c5f63adc1a527cca2dc4bc2e6
Денис <gungrayv@gmail.com>
2021-02-11 20:35:02 +0300

    Добавлены статические файлы, добавлен стиль страниц

--- hello/views.py
+++ hello/views.py
@@ -4,15 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    print("http://127.0.0.1:5000/hello/VSCode")
-    return HttpResponse("Hello Django!")
+    return render(request, "hello/home.html")
 
-def hello_there(request, name):
-    return render(
-        request,
-        'hello/hello_there.html',
-        {
-            'name': name,
-            'date': datetime.now()
-        }
-    )
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
